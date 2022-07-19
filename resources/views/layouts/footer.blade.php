928c617979c9d5b513eba9c534ce8ad26b76e23b
azzahracahyalaraswati <azzahracahyalaraswati@gmail.com>
2022-07-19 17:15:37 +0700

    tugas uas

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <hr />
+            <p>&copy Copyright Toko Laras Powered by DevLover - 2022</p>
+        </div>
+    </div>
+</div>
